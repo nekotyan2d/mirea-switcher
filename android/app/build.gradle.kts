7ea8e6f2519f8d419ea1b8e7aef5d0be1d0095d0
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2026-05-02 01:43:59 +0300

    style: change icon, title and package id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "ru.nekotyan2d.mirea_switcher.mirea_switcher"
+    namespace = "ru.nekotyan2d.mirea_switcher"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ru.nekotyan2d.mirea_switcher.mirea_switcher"
+        applicationId = "ru.nekotyan2d.mirea_switcher"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
